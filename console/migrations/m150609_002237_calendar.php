287e4cb7ac55a88fd2d9527b8b336ca3ac0e45a7
Леонид Некин <kmzslroot@gmail.com>
2015-06-11 05:55:20 +0300

    Миграция для рок-календаря

---
+++ console/migrations/m150609_002237_calendar.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150609_002237_calendar extends Migration
+{
+    public function up()
+    {
+        $this->createTable(
+            '{{%calendar}}',
+            [
+				'id'         	=> 'int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY',
+				'date'		 	=> 'DATE NOT NULL',
+				'description' 	=> 'varchar(255) NOT NULL',
+            ]
+        );
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%calendar}}');
+    }
+    
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+    
+    public function safeDown()
+    {
+    }
+    */
+}
