dab5cc652758873abdcc884dfa7911f97c40bf6d
Леонид Некин <kmzslroot@gmail.com>
2015-06-11 10:10:48 +0300

    'Модели

---
+++ common/models/RockCalendar.php
@@ -0,0 +1,15 @@
+<?php
+namespace common\models;
+
+class RockCalendar extends \yii\db\ActiveRecord
+{
+    public static function model($className=__CLASS__)
+    {
+        return parent::model($className);
+    }
+
+    public static function tableName()
+    {
+        return 'calendar';
+    }
+}
