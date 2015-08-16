14f1c49ff464b58d2c41a6d05bcbac81215f29de
Леонид Некин <kmzslroot@gmail.com>
2015-08-16 23:44:46 +0300

    Rbac

--- common/config/main.php
+++ common/config/main.php
@@ -3,6 +3,16 @@ return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'name' => 'Рок-Портал EQUILIBRIUM',
     'components' => [
+		'authManager' => [
+			'class' => 'yii\rbac\DbManager',
+			 'defaultRoles' => [
+                'administrator',
+                'leading',
+                'article',
+                'partner',
+				'user'
+            ],
+		],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
