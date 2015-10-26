dbdbe543729786b351280604a049e6fe33f344cb
Andrey Shubin <ogursan@yandex.ru>
2015-10-26 22:35:27 +0300

    Поправлен доступ к календарным событиям

--- backend/controllers/CalendarEventController.php
+++ backend/controllers/CalendarEventController.php
@@ -16,14 +16,17 @@ class CalendarEventController extends Controller
 {
     public function behaviors()
     {
-        return [
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'delete' => ['POST'],
+        return array_merge(
+            parent::behaviors(),
+            [
+                'verbs' => [
+                    'class'   => VerbFilter::className(),
+                    'actions' => [
+                        'delete' => ['post'],
+                    ],
                 ],
-            ],
-        ];
+            ]
+        );
     }
 
     /**
